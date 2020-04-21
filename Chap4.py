a1f661597de1b0e49dc3a09b59fdea3f5d422513
SirajKarim <msirajakarim437@gmail.com>
2020-04-21 15:49:40 +0500

    Working with for loop

--- Chap4.py
+++ Chap4.py
@@ -43,6 +43,35 @@ for i in range(1,20):
 # 4-4. One Million: Make a list of the numbers from one to one million, and then
 # use a for loop to print the numbers. (If the output is taking too long, stop it by
 # pressing ctrl-C or by closing the output window.
-numbers = list(range(1,1000000))
-for number in numbers:
-    print(number)
+# numbers = list(range(1,1000000))
+# for number in numbers:
+#     print(number)
+
+# 4-5. Summing a Million: Make a list of the numbers from one to one million,
+# and then use min() and max() to make sure your list actually starts at one and
+# ends at one million. Also, use the sum() function to see how quickly Python can
+# add a million numbers.
+li = list(range(1,1000000))
+print(min(li))
+print(max(li))
+print(sum(li))
+# 4-6. Odd Numbers: Use the third argument of the range() function to make a list
+# of the odd numbers from 1 to 20. Use a for loop to print each number.
+for i in range(0,20,2):
+    print(i)
+# 4-7. Threes: Make a list of the multiples of 3 from 3 to 30. Use a for loop to
+# print the numbers in your list.
+l = [i*3 for i in range(3,30)]
+for i in l:
+    print(i)
+# 4-8. Cubes: A number raised to the third power is called a cube. For example,
+# the cube of 2 is written as 2**3 in Python. Make a list of the first 10 cubes (that
+# is, the cube of each integer from 1 through 10), and use a for loop to print out
+# the value of each cube.
+l = [i**3 for i in range(3,30)]
+for i in l:
+    print(i)
+# 4-9. Cube Comprehension: Use a list comprehension to generate a list of the
+# first 10 cubes
+lm = [i**3 for i in range(1,10)]
+print(lm)
