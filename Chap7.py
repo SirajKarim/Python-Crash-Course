190b68a32e5731c97b79d0507f72c2c3f3784d4c
SirajKarim <msirajakarim437@gmail.com>
2020-06-05 06:48:01 +0500

    ch7

---
+++ Chap7.py
@@ -0,0 +1,60 @@
+# 7-1. Rental Car: Write a program that asks the user what kind of rental car they
+# would like. Print a message about that car, such as “Let me see if I can find you
+# a Subaru.”
+# message = input("what kind of rental car they would like.\n")
+# print(message)
+# 7-2. Restaurant Seating: Write a program that asks the user how many people
+# are in their dinner group. If the answer is more than eight, 
+# print a message saying they’ll have to wait for a table.
+#  Otherwise, report that their table is ready.
+# ques = int(input("How many people are in the dinner:  "))
+# if(ques>8):
+#     print("they’ll have to wait for a table")
+# else:
+#     print("table is ready")
+
+# # While loop 
+# current_number = 1
+# while current_number <= 5:
+#  print(current_number)
+#  current_number += 1
+
+# numb = 1
+# while numb<=10:
+#     print(numb)
+#     numb+=1
+
+# prompt = "\nTell me something, and I will repeat it back to you:"
+# prompt += "\nEnter 'quit' to end the program. "
+# message = ""
+# while message != 'quit':
+#     message = input(prompt)
+#     print(message)
+
+# Using Flag
+
+# active = True
+# while active:
+#     message = input("Enter Something \n")
+#     if message == 'quit':
+#         active = False
+#     else:
+#         print(message)
+
+# while True:
+#     city = input("Enter Your City")
+#     if city == 'quit' :
+#         break
+#     else:
+#         print(city)
+
+# 7-4. Pizza Toppings: Write a loop that prompts the user to enter a series of
+# pizza toppings until they enter a 'quit' value. As they enter each topping,
+# print a message saying you’ll add that topping to their pizza.
+
+# while True:
+#     msg = input("Enter Pizza Topping \n")
+#     if msg == 'quit':
+#         break
+#     else:
+#         print(msg)
